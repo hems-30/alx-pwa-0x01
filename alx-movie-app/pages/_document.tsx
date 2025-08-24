4e75b6d52b72ed541b7546a4bf03875519e09b9d
Hemen Solomon <hemensolomon83@gmail.com>
2025-08-24 14:04:37 +0300

    task 3

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to your PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Theme color for browser UI */}
+        <meta name="theme-color" content="#FFFFFF" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
